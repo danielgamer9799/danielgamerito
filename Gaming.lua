720d8d14db78be1f06cb8160f9376b3a2a9e38da
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2022-08-10 18:52:28 -0400

    Update Gaming.lua

--- Gaming.lua
+++ Gaming.lua
@@ -1,13 +1,29 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Player = game.Players.LocalPlayer
 
-if game.Players.LocalPlayer.UserId == 3662135385 then
-    HidePremium = false
-end
+ScriptHubTab:AddButton({
+	Name = "Bolts Ware V8",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/fusiongreg/BoltsHubV5/main/Main"))();
+        -- credits to bolts
+  	end    
+})
 
-if game.Players.LocalPlayer.UserId == 3789309809 then
-    HidePremium = false
-end
+ScriptHubTab:AddButton({
+	Name = "Eclipse Hub",
+	Callback = function()
+        getgenv().mainKey = "nil"
+        local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https://api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
+  	end    
+})
+
+ScriptHubTab:AddButton({
+	Name = "VG Hub",
+	Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))()
+  	end    
+})
+
 
 local Window = OrionLib:MakeWindow({Name = "Gaming😎 (1.0.0)", HidePremium = false, SaveConfig = true, IntroEnabled = true, IntroText = "Gaming 1.0.0", InfoIcon = "rbxassetid://9866853914", ConfigFolder = "Gaming" })
 
@@ -17,7 +33,7 @@ local CharacterTab = Window:MakeTab({
 	PremiumOnly = false
 })
 
-local CharacterTab = Window:MakeTab({
+local ScriptHubTab = Window:MakeTab({
 	Name = "Script Hubs",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
@@ -25,7 +41,7 @@ local CharacterTab = Window:MakeTab({
 
 OrionLib:MakeNotification({
 	Name = "Hi there!",
-	Content = "Logged in as "..Player.Name.." ".."!",
+	Content = "Logged in as "..Player.Name.." ",
 	Image = "rbxassetid://4483345998",
 	Time = 5
 })
