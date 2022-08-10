bd7a7deb0e90ac290b8784a71ac0f3a4d0e7251d
Danielgamer9799 <110137940+danielgamer9799@users.noreply.github.com>
2022-08-10 18:38:38 -0400

    Update Gaming.lua

--- Gaming.lua
+++ Gaming.lua
@@ -1,9 +1,15 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 local Player = game.Players.LocalPlayer
 
+if game.Players.LocalPlayer.UserId == 3662135385 then
+    HidePremium = false
+end
 
+if game.Players.LocalPlayer.UserId == 3789309809 then
+    HidePremium = false
+end
 
-local Window = OrionLib:MakeWindow({Name = "Gaming😎 (1.0.0)", HidePremium = false, SaveConfig = true, IntroEnabled = true, IntroText = "Gaming 1.0.0", InfoIcon = "rbxassetid://9866853914", })
+local Window = OrionLib:MakeWindow({Name = "Gaming😎 (1.0.0)", HidePremium = false, SaveConfig = true, IntroEnabled = true, IntroText = "Gaming 1.0.0", InfoIcon = "rbxassetid://9866853914", ConfigFolder = "Gaming" })
 
 local CharacterTab = Window:MakeTab({
 	Name = "Character",
@@ -19,7 +25,7 @@ local CharacterTab = Window:MakeTab({
 
 OrionLib:MakeNotification({
 	Name = "Hi there!",
-	Content = "Logged in as ",
+	Content = "Logged in as "..Player.Name.." ",
 	Image = "rbxassetid://4483345998",
 	Time = 5
 })
